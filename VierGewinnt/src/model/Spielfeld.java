c5e29275bc699d9fe32206fadffacd9daa252e19
L0c0F1x <37844657+L0c0F1x@users.noreply.github.com>
2018-05-03 22:30:39 +0200

    Kommentare als Hilfestellungen ergänzt
    Schau mal drüber, ob dir das weiterhilft. Ansonsten meld dich gerne
    nochmal bzw. lass wieder eine Nachricht da :)

--- VierGewinnt/src/model/Spielfeld.java
+++ VierGewinnt/src/model/Spielfeld.java
@@ -39,6 +39,19 @@ public class Spielfeld {
 
 	}
 
+	// Die Klasse Spielfeld sollte nicht direkt für die Darstellung des Spielfelds zuständig sein.
+	// Stattdessen sollte die SpielEngine dies erledigen (das ist wichtig im Hinblick auf ein spätere Ausgliederung
+	// der Darstellungslogik).
+	// Schau nochmal in das Klassendiagramm und achte darauf, wo welche Methoden definiert sind.
+	// Konkret ist in der Spielfeld-Klasse die Methode "getSpielfeldAnsicht" definiert. 
+	// In der SpielEngine-Klasse ist die private Methode "zeigeSpielfeld" definiert.
+	// 
+	// Diese Methoden spielen folgendermaßen zusammen:
+	// Die Methode "getSpielfeldAnsicht" soll eine Kopie von "arraySpielFeld" zurückgeben, damit es von außen nicht möglich ist, das Spielfeld zu verändern - Prinzip Information Hiding.
+	// (Leider konnte ich den Rückgabetypen im Klassendiagramm nicht definieren. Lass dich davon also nicht beirren)
+	// Wenn dir das zu kompliziert scheint, dann kann auch zunächst einfach das arraySpielFeld zurückgegeben werden.
+	// Die SpielEngine-Klasse holt sich dann in der "zeigeSpielfeld"-Methode die Ansicht/Kopie dieses Arrays und gibt es auf der Konsole aus.
+
 	public void SpielfeldDarstellen(Farbe[][] arraySpielfeld) {
 		for (int i = 0; i < arraySpielfeld.length; i++) {
 			for (int j = 0; j < arraySpielfeld[i].length; j++)
--- VierGewinnt/src/model/VierGewinntMain.java
+++ VierGewinnt/src/model/VierGewinntMain.java
@@ -5,6 +5,21 @@ public class VierGewinntMain {
 	// Müssen enums als Objekt erstellt werden? Nein, es kann statisch darauf
 	// zugegriffen werden.
 
+	// Ich gehe hier auf deinen Commit-Kommentar bzgl. dem Aufrufen der start-Methode aus der SpielEngine ein.
+	//
+	// Zunächst nochmal der Hinweis:
+	// Jegliche "public"-Methoden können immer nur an einem konkreten Objekt aufgerufen werden. 
+	// Nur "public static"-Methoden können direkt an der Klasse aufgerufen werden, ohne vorher ein Objekt der Klasse erzeugt zu haben.
+	// 
+	// Konkret heißt das nun:
+	// 1. Die Methode "start" ist nur "public" NICHT "static", also muss zunächst ein Objekt von "SpielEngine" erzeugt werden.
+	// 2. Erstelle dieses Objekt von "SpielEngine" per "new SpielEngine()"
+	// 3. Rufe die "start"-Methode an diesem Objekt auf.
+	//
+	// Beispiel-Code (nur innerhalb einer Methode gültig, z. B. innerhalb der "main"):
+	// SpielEngine spielEngine = new SpielEngine(); // Objekt erstellen
+	// spielEngine.start(); 			
+
 	public static void main(String[] args) {
 
 	}
