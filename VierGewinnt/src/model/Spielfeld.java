e7c6b0edcc08cc45f899e9345546bbe33059ee28
cram <cram@192.168.0.101>
2018-05-15 14:11:34 +0200

    Abfang der Eingaben über den Scannner, veränderter Winalgorithmus

--- VierGewinnt/src/model/Spielfeld.java
+++ VierGewinnt/src/model/Spielfeld.java
@@ -51,6 +51,38 @@ public class Spielfeld {
 		}
 	}
 
+	public boolean pruefeObVierInZeile(Farbe[][] arraySpielFeld) {
+		for (int zeile = 0; zeile < 6; zeile++) {
+			int summe = 1;
+			for (int spalte = 0; spalte < 6; spalte++) { // nur bis zur 6.Spalte pruefen, da die nachfolgende Spalte
+															// immer mitkontrolliert wird
+				if (arraySpielFeld[zeile][spalte + 1] != null && arraySpielFeld[zeile][spalte] != null
+						&& arraySpielFeld[zeile][spalte] == arraySpielFeld[zeile][spalte + 1]) {
+					summe++;
+
+					// Wie soll diese Bedingung (summe >= 4) an dieser Stelle jemals wahr werden?
+					// summe wird oben auf 1 gesetzt und bei dem Wert bleibt es dann auch.
+					// Ich würde an dieser Stelle unkonditional (also ohne if-Block) summe = summe +
+					// 1 oder auch einfach summe++ (ist die kurze Notation dafür) setzen.
+					// So wird jedes Mal der Wert erhöht, wenn ein gleicher Stein als Nachbar
+					// gefunden wird.
+					// Sollte kein gleicher Nachbar gefunden werden, dann summe = 1.
+					// Nachdem die innere Schleife für die Spalten durchgelaufen ist, kannst du dann
+					// die Bedingung (summe >= 4) verwenden um true zurückzugeben. Also:
+
+				} else { // Diesen Block als else-Zweig für den (arraySpielfeld[zeile][spalte] != null &&
+							// ...)-Block (also den äußeren if-Block) verwenden
+					summe = 1;
+				}
+			}
+			if (summe >= 4) { // Diesen if-Block hinter die innere Schleife setzen
+				return true;
+			}
+		}
+		return false;
+
+	}
+
 	public boolean VierEineReihe() {
 
 		return false;
--- VierGewinnt/src/model/VierGewinntMain.java
+++ VierGewinnt/src/model/VierGewinntMain.java
@@ -4,7 +4,6 @@ public class VierGewinntMain {
 
 	public static void main(String[] args) {
 		SpielEngine test = new SpielEngine();
-		// Hier muss ein Objekt von SpielEngine erzeugt werden
 		test.start();
 
 	}
